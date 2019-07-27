3bb8e8e8239b813a6fb68af6af332ce501f49959
L07152055 <365165766@qq.com>
2019-07-27 20:06:31 +0800

    use application.yml data

--- src/main/java/com/majiang/community/controller/AuthorizeController.java
+++ src/main/java/com/majiang/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.majiang.community.dto.AccessTokeDTO;
 import com.majiang.community.dto.GithubUser;
 import com.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,15 +14,22 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state) {
         AccessTokeDTO accessTokeDTO = new AccessTokeDTO();
-        accessTokeDTO.setClient_id("bab8e840f32b2660e3b0");
+        accessTokeDTO.setClient_id(clientId);
         accessTokeDTO.setCode(code);
         accessTokeDTO.setState(state);
-        accessTokeDTO.setClient_secret("5f1ec6b96cffc46f8af5d386aee6f584f87ff056");
-        accessTokeDTO.setRedirect_uri("http://localhost:8888/callback");
+        accessTokeDTO.setClient_secret(clientSecret);
+        accessTokeDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokeDTO);
         GithubUser user = githubProvider.getUser(accessToken);
 
