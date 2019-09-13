58aa90e75dfaffee480895940c18aa2a3d202c36
ZTE10254298 <365165766@qq.com>
2019-09-13 15:25:04 +0800

    session and cookie

--- README.md
+++ README.md
@@ -12,3 +12,17 @@
 [OKHttp](https://square.github.io/okhttp/)
 
 ### 工具
+
+
+### 脚本
+
+```sql
+CREATE CACHED TABLE PUBLIC.USER(
+    ID INTEGER NOT NULL,
+    ACCOUNT_ID VARCHAR(100),
+    NAME VARCHAR(50),
+    TOKEN CHAR(36),
+    GMT_CREATE BIGINT,
+    GMT_MODIFIED BIGINT
+)
+```
--- src/main/java/com/majiang/community/mapper/UserMapper.java
+++ src/main/java/com/majiang/community/mapper/UserMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-
     @Insert("INSERT INTO user (name,account_id,token,gmt_create,gmt_modified) VALUES (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     public void insert(User user);
 }
